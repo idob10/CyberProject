421d9520521cd7ac97bf9f78f9cebc4e24c9f2ce
Yogev173 <a0585324482@gmail.com>
2021-10-25 13:55:27 +0300

    mission 1.1

--- ex1/src/client.py
+++ ex1/src/client.py
@@ -3,7 +3,7 @@ import socket
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    s.sendto(b'Yogev Abarbanel - 326116910, Ido Barkai - ', ('127.0.0.1', 12345))
+    s.sendto(b'Yogev Abarbanel - 326116910, Ido Barkai - 326629987', ('127.0.0.1', 12345))
     data, addr = s.recvfrom(1024)
     print(str(data), addr)
 
