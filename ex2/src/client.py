d252acccd6fc0a45929461c96539b0b644d89b66
Yogev173 <a0585324482@gmail.com>
2021-11-23 15:15:24 +0200

    ...

--- ex2/src/client.py
+++ ex2/src/client.py
@@ -5,24 +5,19 @@ from server import NEW_CLIENT_MASSAGE
 from server import CLOSE_CONNECTION
 from server import sendMsg
 from server import getMsg
+from watchdog.observers import Observer
 
-def connect(sock, path, id, directoryApplayer):
-    if id == None:
-        sendMsg(sock, NEW_CLIENT_MASSAGE)
-    else:
-        sendMsg(sock, id)
-
-    id = getMsg(sock)
-
-    msg = getMsg(sock)
-    while msg != utils.PROTOCOL_END_OF_MODIFICATION:
-        directoryApplayer.handleNewModify(msg)
+def connect(sock, path, isNewClient, directoryApplayer):
+    if not isNewClient:
+        msg = getMsg(sock)
+        while msg != utils.PROTOCOL_END_OF_MODIFICATION:
+            directoryApplayer.handleNewModify(msg)
 
     # upload the dir
-    utils.sendDir(sock, path)
+    directoryApplayer.sendDir(path)
 
 
-def updateServer(sock,id, directoryApplayer, modify_queue):
+def updateServer(sock, id, directoryApplayer, modify_queue):
     sendMsg(sock, id)
     id = getMsg(sock)
 
@@ -39,7 +34,7 @@ def updateServer(sock,id, directoryApplayer, modify_queue):
 def main():
     ip = '127.0.0.1'
     port = 12345
-    path = "C:\Yogev\BIU\Second year\Networks\ex2\test"
+    path = "..\\test"
     timeout = 5
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -47,13 +42,26 @@ def main():
     directoryApplayer = utils.DirectoryApplayer(path, sock)
 
     # In case of a new client
-    id = None
-    connect(sock, path, id, directoryApplayer)
+    id = ""
+    isNewClient = False
+    if id == "":
+        sendMsg(sock, NEW_CLIENT_MASSAGE)
+        isNewClient = True
+    else:
+        sendMsg(sock, id)
 
+    id = getMsg(sock)
+    connect(sock, path, isNewClient, directoryApplayer)
+
+    #start observing
     modify_queue = []
     directoryObserver = utils.DirectoryObserver(modify_queue)
+    observer = Observer()
+    observer.schedule(directoryObserver, path, recursive=True)
+    observer.start()
     while True:
         updateServer(sock, id, directoryApplayer, modify_queue)
+        sendMsg(sock, CLOSE_CONNECTION)
         time.sleep(timeout)
 
 
